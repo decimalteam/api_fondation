8862597f5b214997ecd0d6bb43c5f451b9ae3c58
A Code <uchitsa@ya.ru>
2023-07-31 23:54:26 +0300

    add config.go

---
+++ worker/config.go
@@ -0,0 +1,19 @@
+package worker
+
+import "os"
+
+type Config struct {
+	IndexerEndpoint string
+	RpcEndpoint     string
+	Web3Endpoint    string
+	WorkersCount    int
+}
+
+func getConfig() *Config {
+	return &Config{
+		IndexerEndpoint: os.Getenv("INDEXER_URL"),
+		RpcEndpoint:     os.Getenv("RPC_URL"),
+		Web3Endpoint:    os.Getenv("WEB3_URL"),
+		WorkersCount:    1,
+	}
+}
