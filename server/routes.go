98494995f53e34e4a7bf6f1d04e7e6ffc60c3023
hakerkiler <vinatorul2@gmail.com>
2023-08-17 20:11:23 +0300

    same fix

--- server/routes.go
+++ server/routes.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bitbucket.org/decimalteam/api_fondation/logger"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,6 +10,6 @@ type Handler struct {
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
-
+	router.Use(logger.LogGinMiddleware())
 	return router
 }
