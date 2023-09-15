526c176415b866ceb82814df6436e5da313562ab
A Code <uchitsa@ya.ru>
2023-09-15 14:45:38 +0300

    get block from indexer

--- parser/parser_index.go
+++ parser/parser_index.go
@@ -1,7 +1,58 @@
 package parser
 
+import (
+	"bitbucket.org/decimalteam/api_fondation/clients"
+	"bitbucket.org/decimalteam/api_fondation/parser/cosmos"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+)
+
 type IndexData struct {
 	Height  string `json:"height"`
 	Data    string `json:"data"`
 	EvmData string `json:"evmData"`
+}
+
+func getBlockFromIndexer(indexerNode string) (*cosmos.Block, error) {
+	var res *cosmos.Block
+
+	url := fmt.Sprintf("%s/getWork", indexerNode)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Printf("get block from indexer error: %v", err)
+		return res, err
+	}
+
+	hostname, err := clients.GetHostName()
+	if err != nil {
+		fmt.Printf("get hostname error: %v", err)
+		return res, err
+	}
+	req.Header.Set("X-Worker", hostname)
+
+	clients.GetHttpClient()
+	resp, err := clients.GetHttpClient().Do(req)
+	if err != nil {
+		fmt.Printf("get block from indexer error: %v", err)
+		return res, err
+	}
+	defer resp.Body.Close()
+
+	// Parse response
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("get block from indexer error: %v", err)
+		return res, err
+	}
+	height, err := strconv.Atoi(string(bodyBytes))
+	if err != nil {
+		fmt.Printf("get block from indexer error: %v", err)
+		return res, err
+	}
+
+	res = worker.GetBlockResult(int64(height))
+
+	return res, nil
 }
