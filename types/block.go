2e854e9ca71a75126eddbe63331e7eec79b336ef
A Code <uchitsa@ya.ru>
2023-10-05 02:12:43 +0300

    fix warnings

--- types/block.go
+++ types/block.go
@@ -10,23 +10,3 @@ type BlockEVM struct {
 	Uncles       []*web3types.Header  `json:"uncles"`
 	Receipts     []*web3types.Receipt `json:"receipts"`
 }
-
-//type Block struct {
-//	ID                interface{}             `json:"id"`
-//	Header            interface{}             `json:"header"`
-//	Data              BlockData               `json:"data"`
-//	Evidence          interface{}             `json:"evidence"`
-//	LastCommit        interface{}             `json:"last_commit"`
-//	Emission          string                  `json:"emission"`
-//	Rewards           []ProposerReward        `json:"rewards"`
-//	CommissionRewards []CommissionReward      `json:"commission_rewards"`
-//	BeginBlockEvents  []Event                 `json:"begin_block_events"`
-//	EndBlockEvents    []Event                 `json:"end_block_events"`
-//	Size              int                     `json:"size"`
-//	EVM               BlockEVM                `json:"evm"`
-//	StateChanges      events.EventAccumulator `json:"state_changes"`
-//}
-//
-//type BlockData struct {
-//	Txs []Tx `json:"txs"`
-//}
