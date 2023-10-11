bb3d9d95c5062e0c368ca0789360185a3a35815c
A Code <uchitsa@ya.ru>
2023-10-12 02:51:37 +0300

    update parser index

--- pkg/parser/parser_index.go
+++ pkg/parser/parser_index.go
@@ -5,8 +5,6 @@ import (
 	"bitbucket.org/decimalteam/api_fondation/pkg/parser/cosmos"
 	"bitbucket.org/decimalteam/api_fondation/worker"
 	"fmt"
-	"io"
-	"net/http"
 	"strconv"
 )
 
@@ -21,42 +19,7 @@ func getBlockFromIndexer(indexerNode string) (*cosmos.Block, error) {
 
 	url := fmt.Sprintf("%s/getWork", indexerNode)
 	bytes := client.GetRequest(url)
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		fmt.Printf("get block from indexer error: %v", err)
-		return res, err
-	}
 
-	cl, err := client.New()
-	if err != nil {
-		fmt.Printf("create new client error: %v", err)
-		return res, err
-	}
-
-	if err != nil {
-		fmt.Printf("get hostname error: %v", err)
-		return res, err
-	}
-	req.Header.Set("X-Worker", cl.Hostname)
-
-	resp, err := cl.HttpClient.Do(req)
-	if err != nil {
-		fmt.Printf("get block from indexer error: %v", err)
-		return res, err
-	}
-	defer func() {
-		defErr := resp.Body.Close()
-		if defErr != nil {
-			fmt.Printf("http response close error: %v", err)
-		}
-	}()
-
-	// Parse response
-	_, err = io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("get block from indexer error: %v", err)
-		return res, err
-	}
 	height, err := strconv.Atoi(string(bytes))
 	if err != nil {
 		fmt.Printf("get block from indexer error: %v", err)
