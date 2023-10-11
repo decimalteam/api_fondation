df203c001a558f4c5d8c5d679629bc1c83bdd91f
A Code <uchitsa@ya.ru>
2023-10-12 02:52:40 +0300

    update parser

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -109,48 +109,7 @@ func getBlockFromDataSource(address string) (*cosmos.Block, error) {
 	var res *cosmos.Block
 
 	bytes := client.GetRequest(address)
-	_, err := downloadBlockData(address)
-	if err != nil {
-		fmt.Printf("block data request error: %v ", err)
-		return res, err
-	}
 
-	req, err := http.NewRequest("GET", address, nil)
-	if err != nil {
-		fmt.Printf("get block from indexer error: %v", err)
-		return res, err
-	}
-
-	cl, err := client.New()
-	if err != nil {
-		fmt.Printf("create new client error: %v", err)
-		return res, err
-	}
-
-	if err != nil {
-		fmt.Printf("get hostname error: %v", err)
-		return res, err
-	}
-	req.Header.Set("X-Worker", cl.Hostname)
-
-	resp, err := cl.HttpClient.Do(req)
-	if err != nil {
-		fmt.Printf("get block from indexer error: %v", err)
-		return res, err
-	}
-	defer func() {
-		defErr := resp.Body.Close()
-		if defErr != nil {
-			fmt.Printf("http response close error: %v", err)
-		}
-	}()
-
-	// Parse response
-	_, err = io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("get block from indexer error: %v", err)
-		return res, err
-	}
 	height, err := strconv.Atoi(string(bytes))
 	if err != nil {
 		fmt.Printf("get block from indexer error: %v", err)
