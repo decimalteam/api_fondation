577c99f80c77d31ea576219c867f3d7d479cd6d3
hakerkiler <vinatorul2@gmail.com>
2023-10-19 20:34:35 +0300

    fix worker.go

--- worker/worker.go
+++ worker/worker.go
@@ -31,7 +31,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func GetBlockResult(height int64) *types.BlockData {
+func GetBlockResult(height int64, withTrx bool) *types.BlockData {
 	ctx := context.Background()
 
 	accum := events.NewEventAccumulator()
@@ -64,29 +64,31 @@ func GetBlockResult(height int64) *types.BlockData {
 	go FetchBlockWeb3(ctx, cl.Web3Client, height, web3BlockChan)
 
 	web3Block := <-web3BlockChan
-	go FetchBlockTxReceiptsWeb3(cl.EthRpcClient, web3Block, web3ReceiptsChan)
-	web3Body := web3Block.Body()
-	web3Transactions := make([]*evm.TransactionEVM, len(web3Body.Transactions))
-	for i, tx := range web3Body.Transactions {
-		msg, err := tx.AsMessage(web3types.NewLondonSigner(cl.Web3ChainId), nil)
-		panicError(err)
-		web3Transactions[i] = &evm.TransactionEVM{
-			Type:             web3hexutil.Uint64(tx.Type()),
-			Hash:             tx.Hash(),
-			Nonce:            web3hexutil.Uint64(tx.Nonce()),
-			BlockHash:        web3Block.Hash(),
-			BlockNumber:      web3hexutil.Uint64(web3Block.NumberU64()),
-			TransactionIndex: web3hexutil.Uint64(uint64(i)),
-			From:             msg.From(),
-			To:               msg.To(),
-			Value:            (*web3hexutil.Big)(msg.Value()),
-			Data:             msg.Data(),
-			Gas:              web3hexutil.Uint64(msg.Gas()),
-			GasPrice:         (*web3hexutil.Big)(msg.GasPrice()),
-			ChainId:          (*web3hexutil.Big)(tx.ChainId()),
-			AccessList:       msg.AccessList(),
-			GasTipCap:        (*web3hexutil.Big)(msg.GasTipCap()),
-			GasFeeCap:        (*web3hexutil.Big)(msg.GasFeeCap()),
+	if withTrx {
+		go FetchBlockTxReceiptsWeb3(cl.EthRpcClient, web3Block, web3ReceiptsChan)
+		web3Body := web3Block.Body()
+		web3Transactions := make([]*evm.TransactionEVM, len(web3Body.Transactions))
+		for i, tx := range web3Body.Transactions {
+			msg, err := tx.AsMessage(web3types.NewLondonSigner(cl.Web3ChainId), nil)
+			panicError(err)
+			web3Transactions[i] = &evm.TransactionEVM{
+				Type:             web3hexutil.Uint64(tx.Type()),
+				Hash:             tx.Hash(),
+				Nonce:            web3hexutil.Uint64(tx.Nonce()),
+				BlockHash:        web3Block.Hash(),
+				BlockNumber:      web3hexutil.Uint64(web3Block.NumberU64()),
+				TransactionIndex: web3hexutil.Uint64(uint64(i)),
+				From:             msg.From(),
+				To:               msg.To(),
+				Value:            (*web3hexutil.Big)(msg.Value()),
+				Data:             msg.Data(),
+				Gas:              web3hexutil.Uint64(msg.Gas()),
+				GasPrice:         (*web3hexutil.Big)(msg.GasPrice()),
+				ChainId:          (*web3hexutil.Big)(tx.ChainId()),
+				AccessList:       msg.AccessList(),
+				GasTipCap:        (*web3hexutil.Big)(msg.GasTipCap()),
+				GasFeeCap:        (*web3hexutil.Big)(msg.GasFeeCap()),
+			}
 		}
 	}
 
