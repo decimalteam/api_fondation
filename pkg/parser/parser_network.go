9643ee6c0cdef95be48549de640cecde3dbdd1c0
hakerkiler <vinatorul2@gmail.com>
2023-10-23 13:01:55 +0300

    fix worker.go

--- pkg/parser/parser_network.go
+++ pkg/parser/parser_network.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"bitbucket.org/decimalteam/api_fondation/pkg/parser/evm"
 	"bitbucket.org/decimalteam/api_fondation/worker"
 )
 
@@ -12,4 +13,8 @@ type NetworkData struct {
 
 func (p *Parser) getBlockFromNetwork(height int64, withTrx bool) {
 	p.ChanelNewBlock = worker.GetBlockResult(height, withTrx)
+}
+
+func (p *Parser) getEvmBlock(height int64) *evm.BlockEVM {
+	return worker.GetEvmBlock(height)
 }
